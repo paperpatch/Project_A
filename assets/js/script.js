7e181d93847a7080d4ac087e9770f788255d301a
paperpatch <patchen21@gmail.com>
2021-11-02 15:13:14 -0400

    made search setLocalStorage and redirect to other html pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,12 @@ var formSubmitHandler = function (event) {
 
   // clear search input and old data
   $("#input-search").val("");
-  window.localStorage.removeItem("searchValue")
+  window.localStorage.removeItem("searchRecipe")
 
   // set to localStorage for Discover Recipes HTML Page
-  foodRecipeSearch(searchValue);
+  window.localStorage.setItem("searchRecipe", JSON.stringify(searchValue));
+  // redirect to page
+  window.location.assign('./assets/html/recipes.html')
 }
 
 
@@ -25,9 +27,11 @@ foodForm.addEventListener("submit", formSubmitHandler);
 // Appended Recent Search List for Details HTML Page
 $("#recipes-container").on("click", "li", function () {
   // clear old data
-  window.localStorage.removeItem("searchList")
+  window.localStorage.removeItem("recentRecipe")
   // set localStorage for third html page
   let searchList = $(this).text();
-
+  window.localStorage.setItem("recentRecipe", JSON.stringify(searchList));
+  // redirect to page
+  window.location.assign('./assets/html/detail.html')
 })
 
