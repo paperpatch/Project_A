b838d2bce53511aaa2c1dfbe4b3e8e5828b6bcce
paperpatch <patchen21@gmail.com>
2021-11-02 19:21:25 -0400

    added api for trending recipe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
+var apiKey = "3a44b6d72cmsh2c9491cf44c4730p152adajsn7b494b9925d6";
 var foodForm = document.querySelector("#food-form");
-/* 
+
 // Search Section
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -18,9 +19,31 @@ var formSubmitHandler = function (event) {
   window.location.assign('./assets/html/recipes.html')
 }
 
+var trendingRecipe = function() {
+  fetch("https://tasty.p.rapidapi.com/feeds/list?size=20&timezone=%2B0700&vegetarian=%3CREQUIRED%3E&from=0", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "tasty.p.rapidapi.com",
+      "x-rapidapi-key": apiKey,
+    }
+  })
+  .then(response => {
+    console.log(response);
+    if (response.ok) {
+      response.json().then(function (data) {
 
-// Event Listener Section
-foodForm.addEventListener("submit", formSubmitHandler);
+        getRecipeDetail(data);
+      })
+    } else {
+      // need to change this alert to modal
+      alert("Error: " + response.statusText)
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+
 
 // Appended Recent Search List for Details HTML Page
 $("#recipes-container").on("click", "li", function () {
@@ -32,4 +55,6 @@ $("#recipes-container").on("click", "li", function () {
   // redirect to page
   window.location.assign('./assets/html/detail.html')
 })
- */
+
+// Event Listener Section
+foodForm.addEventListener("submit", formSubmitHandler);
