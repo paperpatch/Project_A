73822c20a7c686b2ce4749b8445381d880bf07f1
BrandonConte <bconte950@gmail.com>
2021-11-02 17:12:45 -0400

    Revert "Merge branch 'develop' into Recipe"
    This reverts commit f428be679a6b1fec75985e99b830f56754ec960e, reversing
    changes made to f173c5995e24fed9b912dba753eb3c538a51ff0c.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,39 +11,3 @@
 // .then(function(data){
 //     console.log(data);
 // })
-
-var foodForm = $("#food-form");
-
-// Search Section
-var formSubmitHandler = function (event) {
-  event.preventDefault();
-
-  // get value from input element
-  let searchInput = foodForm.value.trim();
-  let searchValue = searchInput.toLowerCase();
-
-  // clear search input and old data
-  $("#input-search").val("");
-  window.localStorage.removeItem("searchRecipe")
-
-  // set to localStorage for Discover Recipes HTML Page
-  window.localStorage.setItem("searchRecipe", JSON.stringify(searchValue));
-  // redirect to page
-  window.location.assign('./assets/html/recipes.html')
-}
-
-
-// Event Listener Section
-foodForm.addEventListener("submit", formSubmitHandler);
-
-// Appended Recent Search List for Details HTML Page
-$("#recipes-container").on("click", "li", function () {
-  // clear old data
-  window.localStorage.removeItem("recentRecipe")
-  // set localStorage for third html page
-  let searchList = $(this).text();
-  window.localStorage.setItem("recentRecipe", JSON.stringify(searchList));
-  // redirect to page
-  window.location.assign('./assets/html/detail.html')
-})
-
