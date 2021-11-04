543ec4f3658088be2065b2bc0c36bc7cf81af76f
paperpatch <patchen21@gmail.com>
2021-11-03 22:30:59 -0400

    added/deleted info based on Tasty's API. deleted ingredients list

--- assets/js/details.js
+++ assets/js/details.js
@@ -25,33 +25,49 @@ var foodDetail = function() {
 
 var getRecipeDetail = function(data) {
   console.log(data);
-// get variables
 
-  let foodName = data.name;
+  // get variables
   let foodImg = data.thumbnail_url;
-  let foodRating = data[i].user_ratings.count_positive;
-  let foodScore = data[i].user_ratings.score;
+  let foodName = data.name;
+  
+  if (data.cook_time_minutes === null || data.cook_time_minutes === 0) {
+    var foodTime = "N/A";
+  } else {
+    var foodTime = String(data.cook_time_minutes);
+  }
+  if (data.num_servings === null || data.num_servings === 0) {
+    var foodServings = "N/A";
+  } else {
+    var foodServings = String(num_servings);
+  }
+  console.log(foodImg);
+  console.log(foodName);
+  console.log(foodTime);
+  console.log(foodServings);
 
+  // create instruction for each [i]
+  // for (let i=0; i < data.length; i++) {
+
+  // }
   // console.log(foodID);
 
-  // create card for each [i]
-  let trendingCard = $("<div>").addClass("card small-3").attr("id", foodID);
-  let trendingSection = $("<div>").addClass("card-section");
-  let trendingImg = $("<img>").attr("src", foodImg).addClass("trending-img");
-  let trendingSection2 = $("<div>").addClass("card-section");
-  let trendingName = $("<h5>").text(foodName);
-  let trendingRating = $("<p>").text("Liked: " + foodRating);
-  let trendingScore = $("<p>").text((foodScore * 100).toFixed(2) + "%");
+  
+  // let trendingCard = $("<div>").addClass("card small-3").attr("id", foodID);
+  // let trendingSection = $("<div>").addClass("card-section");
+  // let trendingImg = $("<img>").attr("src", foodImg).addClass("trending-img");
+  // let trendingSection2 = $("<div>").addClass("card-section");
+  // let trendingName = $("<h5>").text(foodName);
+  // let trendingRating = $("<p>").text("Liked: " + foodRating);
+  // let trendingScore = $("<p>").text((foodScore * 100).toFixed(2) + "%");
 
-  console.log(trendingCard);
+  // console.log(trendingCard);
   // append cards
-  $(".trendingRecipes").append(trendingCard);
-  trendingCard.append(trendingSection, trendingSection2);
-  trendingSection.append(trendingImg);
-  trendingSection2.append(trendingName, trendingRating, trendingScore);
+  // $(".trendingRecipes").append(trendingCard);
+  // trendingCard.append(trendingSection, trendingSection2);
+  // trendingSection.append(trendingImg);
+  // trendingSection2.append(trendingName, trendingRating, trendingScore);
 
-  for (let i=0; i < data.length; i++) {
-  }
+  
   // add recipe detail function using `for loop`[i] and appending it to the list. Max a max limit?
 }
 
@@ -65,28 +81,28 @@ var foodID = "4524"; // Need to pull this data from recipe food.
 foodDetail();
 
 
-var food = "Ultimate Green Bean Casserole"
-var nutritionData = function() {
-  fetch("https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=" + food, {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "nutrition-by-api-ninjas.p.rapidapi.com",
-      "x-rapidapi-key": "5eedb034a3msha6329e8ee03862bp1ded91jsn5333cfd314b9"
-    }
-  })
-  .then(response => {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        console.log(data);
-      })
-    } else {
-      // need to change this alert to modal
-      alert("Error: " + response.statusText)
-    }
-  })
-  .catch(err => {
-    console.error(err);
-  });
-}
-recipe();
+// var food = "Ultimate Green Bean Casserole"
+
+// var fetchNutrition = function() {
+//   fetch("https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=" + food, {
+//     "method": "GET",
+//     "headers": {
+//       "x-rapidapi-host": "nutrition-by-api-ninjas.p.rapidapi.com",
+//       "x-rapidapi-key": "5eedb034a3msha6329e8ee03862bp1ded91jsn5333cfd314b9"
+//     }
+//   })
+//   .then(response => {
+//     if (response.ok) {
+//       response.json().then(function (data) {
+//         console.log(data);
+//       })
+//     } else {
+//       // need to change this alert to modal
+//       alert("Error: " + response.statusText)
+//     }
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+// }
+// fetchNutrition();
