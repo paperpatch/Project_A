b1a9352bfaabe94d98588d57444aeed4fd7dbd99
paperpatch <patchen21@gmail.com>
2021-11-03 21:46:21 -0400

    added ninja-api for nutrition values

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,5 +1,4 @@
 var apiKey = "3a44b6d72cmsh2c9491cf44c4730p152adajsn7b494b9925d6";
-var foodID = "4524"; // Need to pull this data from something.
 
 var foodDetail = function() {
   fetch("https://tasty.p.rapidapi.com/recipes/detail?id=" + foodID, {
@@ -10,11 +9,9 @@ var foodDetail = function() {
     }
   })
   .then(response => {
-    console.log(response);
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
-        // getRecipeDetail(data, input);
+        getRecipeDetail(data);
       })
     } else {
       // need to change this alert to modal
@@ -26,16 +23,70 @@ var foodDetail = function() {
   });
 }
 
-var getRecipeDetail = function(data, input) {
+var getRecipeDetail = function(data) {
+  console.log(data);
+// get variables
 
-  console.log(data, input);
+  let foodName = data.name;
+  let foodImg = data.thumbnail_url;
+  let foodRating = data[i].user_ratings.count_positive;
+  let foodScore = data[i].user_ratings.score;
 
+  // console.log(foodID);
+
+  // create card for each [i]
+  let trendingCard = $("<div>").addClass("card small-3").attr("id", foodID);
+  let trendingSection = $("<div>").addClass("card-section");
+  let trendingImg = $("<img>").attr("src", foodImg).addClass("trending-img");
+  let trendingSection2 = $("<div>").addClass("card-section");
+  let trendingName = $("<h5>").text(foodName);
+  let trendingRating = $("<p>").text("Liked: " + foodRating);
+  let trendingScore = $("<p>").text((foodScore * 100).toFixed(2) + "%");
+
+  console.log(trendingCard);
+  // append cards
+  $(".trendingRecipes").append(trendingCard);
+  trendingCard.append(trendingSection, trendingSection2);
+  trendingSection.append(trendingImg);
+  trendingSection2.append(trendingName, trendingRating, trendingScore);
+
+  for (let i=0; i < data.length; i++) {
+  }
   // add recipe detail function using `for loop`[i] and appending it to the list. Max a max limit?
 }
 
-
-
-// Load Recent Detail
-// var detailStorage = JSON.parse(window.localStorage.getItem("recentRecipe")) || [];
+var detailStorage = JSON.parse(window.localStorage.getItem("searchRecipe")) || [];
 // foodDetail(detailStorage);
+
+// // Load Recent Detail
+// var detailStorage = JSON.parse(window.localStorage.getItem("recentRecipe")) || [];
+
+var foodID = "4524"; // Need to pull this data from recipe food.
 foodDetail();
+
+
+var food = "Ultimate Green Bean Casserole"
+var nutritionData = function() {
+  fetch("https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=" + food, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "nutrition-by-api-ninjas.p.rapidapi.com",
+      "x-rapidapi-key": "5eedb034a3msha6329e8ee03862bp1ded91jsn5333cfd314b9"
+    }
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (data) {
+        console.log(data);
+      })
+    } else {
+      // need to change this alert to modal
+      alert("Error: " + response.statusText)
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+recipe();
