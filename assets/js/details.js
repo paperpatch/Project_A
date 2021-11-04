c55403ec6ac39a07d0bb6de80da9d9e86629222c
paperpatch <patchen21@gmail.com>
2021-11-04 00:20:08 -0400

    added script to append recipe instructions

--- assets/js/details.js
+++ assets/js/details.js
@@ -28,8 +28,7 @@ var getRecipeDetail = function(data) {
 
   // get variables
   let foodImg = data.thumbnail_url;
-  let foodName = data.name;
-  
+  var foodName = data.name;
   if (data.cook_time_minutes === null || data.cook_time_minutes === 0) {
     var foodTime = "N/A";
   } else {
@@ -38,71 +37,62 @@ var getRecipeDetail = function(data) {
   if (data.num_servings === null || data.num_servings === 0) {
     var foodServings = "N/A";
   } else {
-    var foodServings = String(num_servings);
+    var foodServings = String(data.num_servings);
   }
-  console.log(foodImg);
-  console.log(foodName);
-  console.log(foodTime);
-  console.log(foodServings);
 
+  // get Nutrition Value from food name by running through Ninja API
+  fetchNutrition(foodName);
+  
+  // create elements
+  let detailSection = $("<div>").addClass("card-section");
+  let detailImg = $("<img>").attr("src", foodImg).addClass("detail-img");
+  let detailName = $("<h5>").text(foodName);
+  let detailTime = $("<p>").text("Time: " + foodTime);
+  let detailServings = $("<p>").text("Servings: " + foodServings);
+  let detailInstructionHeader = $("<h5>").text("Instructions");
+  let detailUnorderedList = $("<ul>");
   // create instruction for each [i]
-  // for (let i=0; i < data.length; i++) {
+  for (let i=0; i < data.instructions.length; i++) {
+    let detailList = $("<li>").text(data.instructions[i].display_text)
+    detailUnorderedList.append(detailList);
+  }
 
-  // }
-  // console.log(foodID);
-
-  
-  // let trendingCard = $("<div>").addClass("card small-3").attr("id", foodID);
-  // let trendingSection = $("<div>").addClass("card-section");
-  // let trendingImg = $("<img>").attr("src", foodImg).addClass("trending-img");
-  // let trendingSection2 = $("<div>").addClass("card-section");
-  // let trendingName = $("<h5>").text(foodName);
-  // let trendingRating = $("<p>").text("Liked: " + foodRating);
-  // let trendingScore = $("<p>").text((foodScore * 100).toFixed(2) + "%");
-
-  // console.log(trendingCard);
-  // append cards
-  // $(".trendingRecipes").append(trendingCard);
-  // trendingCard.append(trendingSection, trendingSection2);
-  // trendingSection.append(trendingImg);
-  // trendingSection2.append(trendingName, trendingRating, trendingScore);
-
-  
-  // add recipe detail function using `for loop`[i] and appending it to the list. Max a max limit?
+  // append elements
+  $("#instruction-list").append(detailSection, detailName, detailTime, detailServings, detailInstructionHeader);
+  detailSection.append(detailImg);
+  detailInstructionHeader.append(detailUnorderedList);
 }
 
-var detailStorage = JSON.parse(window.localStorage.getItem("searchRecipe")) || [];
-// foodDetail(detailStorage);
+
+var fetchNutrition = function(foodName) {
+  fetch("https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=" + food, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "nutrition-by-api-ninjas.p.rapidapi.com",
+      "x-rapidapi-key": "5eedb034a3msha6329e8ee03862bp1ded91jsn5333cfd314b9"
+    }
+  })
+  .then(response => {
+    if (response.ok) {
+      response.json().then(function (data) {
+        getNutritionDetail(data, foodName);
+      })
+    } else {
+      // need to change this alert to modal
+      alert("Error: " + response.statusText)
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+
+fetchNutrition();
+
+
 
 // // Load Recent Detail
-// var detailStorage = JSON.parse(window.localStorage.getItem("recentRecipe")) || [];
+var detailStorage = JSON.parse(window.localStorage.getItem("recentRecipe")) || [];
 
-var foodID = "4524"; // Need to pull this data from recipe food.
+var foodID = "4524"; // Need to pull this data from detailStorage.
 foodDetail();
-
-
-// var food = "Ultimate Green Bean Casserole"
-
-// var fetchNutrition = function() {
-//   fetch("https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=" + food, {
-//     "method": "GET",
-//     "headers": {
-//       "x-rapidapi-host": "nutrition-by-api-ninjas.p.rapidapi.com",
-//       "x-rapidapi-key": "5eedb034a3msha6329e8ee03862bp1ded91jsn5333cfd314b9"
-//     }
-//   })
-//   .then(response => {
-//     if (response.ok) {
-//       response.json().then(function (data) {
-//         console.log(data);
-//       })
-//     } else {
-//       // need to change this alert to modal
-//       alert("Error: " + response.statusText)
-//     }
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
-// }
-// fetchNutrition();
