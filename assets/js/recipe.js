05c4f28c32cbbc02d318795a5466fafb9ee1de9d
Vertis-Z <48894365+Vertis-Z@users.noreply.github.com>
2021-11-09 19:34:47 -0500

    added scroll to top button with smooth scrolling

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -155,3 +155,25 @@ $("#recipeGridTitle").text("Related Recipes: " + recipeStorage)
 // need to replace 'spaces' with %20 for fetch request to work
 var recipeStorageReplaceSpace = recipeStorage.split(" ").join("%20");
 foodRecipeFilter(recipeStorageReplaceSpace);
+
+/* ---------------------- Scroll to top button ---------------------- */
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
